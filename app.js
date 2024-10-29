4040f5df1f433842eb00bec67b7af02d6a557f14
Om Kokate <om.kokate2902@gmail.com>
2024-10-29 23:30:18 +0530

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Enable CORS for all routes
+app.use(cors());
+
 // Simple route to confirm the app is running
 app.get('/', (req, res) => {
-  res.send('Dev branch works! 🎉');
+  res.send('Dev branch works! 🎊🎉');
 });
 
 // Start the server
