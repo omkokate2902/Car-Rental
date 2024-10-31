21558ba72705e55656edd4161a298f6b6a0a3d6d
Om Kokate <om.kokate2902@gmail.com>
2024-10-31 19:02:24 +0530

    added cors again

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 require('dotenv').config(); // Load environment variables at the very top
 
 const express = require('express');
+const cors = require('cors'); // Require the cors package
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 connectDB();
 
 const app = express();
+
+// Use CORS middleware
+app.use(cors()); // Enable all CORS requests
+
 app.use(express.json());
 
 // Main route to confirm the server is working
