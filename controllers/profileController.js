7a47c034a57a50dbd588a24ffb868d84455a0448
Om Kokate <om.kokate2902@gmail.com>
2024-11-05 19:02:39 +0530

    updated get profile api

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,15 +2,15 @@
 const User = require('../models/User');
 
 exports.getProfile = async (req, res) => {
-  try {
-    const user = await User.findOne({ firebaseUserId: req.user.uid });
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+    try {
+      const user = await User.findOne({ firebaseUserId: req.user.uid }).select('email name roles');
+      if (!user) return res.status(404).json({ message: 'User not found' });
+  
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
 
 exports.updateProfile = async (req, res) => {
   const { name, profile } = req.body;
